a48dca36dddc9f040736445583372b60fb7e0182
Filip Bradaric <filip.bradaric@rimac-automobili.com>
2023-05-12 08:35:27 +0200

    feat: add timeout for http request

--- src/DataGenerator/TemperatureGenerator.py
+++ src/DataGenerator/TemperatureGenerator.py
@@ -2,7 +2,7 @@ import requests
 
 def getTemperature(latitude, longitude):
     url = f"https://api.open-meteo.com/v1/forecast?latitude={str(latitude)}&longitude={str(longitude)}&current_weather=true"
-    response = requests.get(url)
+    response = requests.get(url, timeout=5)
 
     if response.ok:
         data = response.json()
